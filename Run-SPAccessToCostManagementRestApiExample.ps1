7878fcaf58ed16419b53affc37c0c8038a027d63
Nick Eales <neales@microsoft.com>
2019-08-22 09:48:23 +1000

    shift files vbetween repos

--- Run-SPAccessToCostManagementRestApiExample.ps1
+++
@@ -1,6 +0,0 @@
-$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
-& "$PSScriptRoot\Run-GetSPTokenAndRunScript.ps1" `
-    -ScriptToCall "$PSScriptRoot\AzureCostManagementRestApiExample.ps1" `
-    -TenantId '61b9f171-e805-4312-94c0-25df1220b4f4' `
-    -clientId 'a0f5ec63-9951-4691-8c35-6d6284cb29b4' `
-    -ClientSecret  '=OUUx4P?8.=dvaDvr5IxILTjzzvI2:q_'
